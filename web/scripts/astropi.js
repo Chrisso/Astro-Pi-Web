37bb1207f85e5862334b02eed9bf731f96f7bb65
Christoph <christoph@stoepel.net>
2015-10-08 14:29:22 +0200

    D3 based history charts.

--- web/scripts/astropi.js
+++ web/scripts/astropi.js
@@ -1,6 +1,52 @@
 
 /// <reference path="../bower_components/jquery/dist/jquery.js" />
 /// <reference path="../bower_components/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="../bower_components/d3/d3.js" />
+
+function onHistoryUpdate(target, json) {
+    $(target).height(($(target).width() / 21) * 9); // resize to wide screen
+
+    var margin = { top: 20, right: 20, bottom: 20, left: 60 };
+    var width = $(target).width() - margin.left - margin.right;
+    var height = $(target).height() - margin.top - margin.bottom;
+
+    var scales = {};
+    scales.x = d3.time.scale().range([0, width]);
+    scales.y = d3.scale.linear().range([height, 0]);
+
+    var axis = {};
+    axis.x = d3.svg.axis().scale(scales.x).orient("bottom");
+    axis.y = d3.svg.axis().scale(scales.y).orient("left");
+
+    var svg = d3.select(target).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var data = [];
+    for (var i = 0; i < json.length; i++) {
+        data.push({
+            date: new Date(json[i][0]),
+            value: json[i][1]
+        });
+    }
+
+    scales.x.domain(d3.extent(data, function (d) { return d.date; }));
+    scales.y.domain(d3.extent(data, function (d) { return d.value; }));
+
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(axis.x);
+    svg.append("g").attr("class", "y axis").call(axis.y);
+
+    var line = d3.svg.line()
+        .x(function (d) { return scales.x(d.date); })
+        .y(function (d) { return scales.y(d.value); });
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+}
 
 function onSensorUpdate(data) {
     $("#sensor_temperature").html(data.temperature.toFixed(1) + "&deg;C");
@@ -17,5 +63,17 @@ function doSensorUpdate() {
 }
 
 $(document).ready(function () {
+    $.getJSON(
+        "services/json/sensors.php",
+        { q: "temperature", ts: new Date().getTime() },
+        function (data) { onHistoryUpdate("#history_temperature", data); });
+    $.getJSON(
+        "services/json/sensors.php",
+        { q: "humidity", ts: new Date().getTime() },
+        function (data) { onHistoryUpdate("#history_humidity", data); });
+    $.getJSON(
+        "services/json/sensors.php",
+        { q: "pressure", ts: new Date().getTime() },
+        function (data) { onHistoryUpdate("#history_pressure", data); });
     doSensorUpdate();
 });
--- web/services/json/sensors.php
+++ web/services/json/sensors.php
@@ -16,11 +16,34 @@
     return json_encode($result, JSON_PRETTY_PRINT);
   }
   
+  function JsonHistory($sensor) {
+    $result = [];
+    $db = new PDO('sqlite:' . $GLOBALS['db_file']);
+    $data = $db->query(sprintf('SELECT [Timestamp], %s FROM [Sensors] ORDER BY [Timestamp] DESC LIMIT 144', $sensor), PDO::FETCH_NUM);
+    foreach ($data as $row) {
+      array_push($result,
+        [date(DATE_ISO8601, strtotime($row[0])),
+         floatval($row[1])]);
+    }
+    $db = null;
+    return json_encode(array_reverse($result), JSON_PRETTY_PRINT);
+  }
+  
   $json = '{ "status": "unknown query" }';
 
   if (isset($_REQUEST['q'])) {
+    date_default_timezone_set('Etc/Universal');
     if (strcasecmp($_REQUEST['q'], 'recent') == 0) {
       $json = JsonRecent();
+    }
+    else if (strcasecmp($_REQUEST['q'], 'temperature') == 0) {
+      $json = JsonHistory('[Temperature]'); // never feed user input directly!
+    }
+    else if (strcasecmp($_REQUEST['q'], 'humidity') == 0) {
+      $json = JsonHistory('[Humidity]');
+    }
+    else if (strcasecmp($_REQUEST['q'], 'pressure') == 0) {
+      $json = JsonHistory('[Pressure]');
     }
   }
 
